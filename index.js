72db2a90582eb91aa1b5cf579a9c4e7ee39e6f91
Adil Ansari <adilansari35830@gmail.com>
2025-08-30 17:52:15 +0530

    for nothing

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.static("public"));
 // CORS: Allow requests from frontend and send cookies
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
